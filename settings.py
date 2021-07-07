12315902019b8a617d1ae89d7c15ba487a15684c
Fw9wef <Sasha252VNS@gmail.com>
2021-07-07 15:19:08 +0300

    1

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-per_gpu_workers = 2
+per_gpu_workers = 8
 gpus = 'cpu'
 lam = 0.95
 gamma = 0.99
 epsilon = 0.2
 iterations = 10000
 ppo_epochs = 10
-batch_size = 32
-n_batch = 4
-n_episodes = 2
+batch_size = 512
+n_batch = 10
+n_episodes = 4
