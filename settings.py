c0a6de59d347131ed7e9681cb1fd291340cc2f5a
Fw9wef <Sasha252VNS@gmail.com>
2021-07-08 23:58:15 +0300

    1

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
-per_gpu_workers = 4
+per_gpu_workers = 8
 gpus = "cpu"
 lam = 0.95
 gamma = 0.99
 epsilon = 0.2
 iterations = 10000
 ppo_epochs = 10
-batch_size = 32
+batch_size = 512
 n_batch = 10
-n_episodes = 1
+n_episodes = 4
 n_steps = 2048
